bd7290f4a782c19a0bcf50337f4eacc83d9c2c7e
rithymeth <rithy1331@gmail.com>
2024-11-24 15:30:45 +0700

    update

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -30,7 +30,7 @@ const Content: React.FC = () => {
         </div>
         <div className="content-right">
           <div className="image-container">
-            <img src="./images/Edison-english.png" alt="ICG Gaming Platform Preview" loading="lazy" />
+            <img src="./images/Gary-enlish.png" alt="ICG Gaming Platform Preview" loading="lazy" />
           </div>
         </div>
       </div>
